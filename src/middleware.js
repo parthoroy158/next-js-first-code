8bca7cf7299fa93df8ef09ddc1ceb271e8dc3e50
Partho Protim Roy <parthoroy158@gmail.com>
2025-06-10 07:06:46 +0600

    get token form cookies

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,19 @@ import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+
+    const currentCookies = request.cookies.get('nextjs-token')
+    console.log(currentCookies.value)
+    const dummyUserData = {
+        role: 'user',
+        email: 'admin123@gmail.com'
+    }
+
+    let isServices = request.nextUrl.pathname.startsWith('/services')
+    let isAdmin = dummyUserData.role === 'admin'
+    if (!isAdmin && isServices) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
     return NextResponse.next()
 }
 
